5de3ffadd23225e1df1a6b9c14af9bbcb7fa698f
vagrant <vagrant@localhost.localdomain>
2019-12-21 06:12:09 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Quazi'
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
